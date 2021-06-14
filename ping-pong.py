07cb41f6ca33836761d1e49ae0f5a033d98fd05a
Rafael Antunes <rafao.lima@hotmail.com>
2021-06-14 17:35:43 -0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-# cores do projeto
+# cores
 PRETO = 0, 0, 0
 BRANCO = 255, 255, 255
 VERDE = 0, 255, 0
@@ -14,17 +14,19 @@ tamanho = 800, 600
 tela = pygame.display.set_mode(tamanho)
 tela_retangulo = tela.get_rect()
 
+
 # Primeira Raquete
 class Raquete1:
     def __init__(self, tamanho):
         self.imagem = pygame.Surface(tamanho)
         self.imagem.fill(VERDE)
         self.imagem_retangulo = self.imagem.get_rect()
-        self.velocidade = 15
+        self.velocidade = 3
+        self.imagem_retangulo[0] = 0
 
     def move(self, x, y):
-        self.imagem_retangulo[0] += x
-        self.imagem_retangulo[1] += y
+        self.imagem_retangulo[0] += x * self.velocidade
+        self.imagem_retangulo[1] += y * self.velocidade
 
     def atualiza(self, tecla):
         if tecla[pygame.K_w]:
@@ -37,18 +39,19 @@ class Raquete1:
     def realiza(self):
         tela.blit(self.imagem, self.imagem_retangulo)
 
+
 # Segunda Raquete
 class Raquete2:
     def __init__(self, tamanho):
         self.imagem = pygame.Surface(tamanho)
         self.imagem.fill(ROXO)
         self.imagem_retangulo = self.imagem.get_rect()
-        self.velocidade = 15
+        self.velocidade = 3
         self.imagem_retangulo[0] = 800
 
     def move(self, x, y):
-        self.imagem_retangulo[0] += x
-        self.imagem_retangulo[1] += y
+        self.imagem_retangulo[0] += x * self.velocidade
+        self.imagem_retangulo[1] += y * self.velocidade
 
     def atualiza(self, tecla):
         if tecla[pygame.K_UP]:
@@ -61,9 +64,11 @@ class Raquete2:
     def realiza(self):
         tela.blit(self.imagem, self.imagem_retangulo)
 
-# Bola        
+
+# Bola
 class Bola:
     def __init__(self, tamanho):
+        self.altura, self.largura = tamanho
         self.imagem = pygame.Surface(tamanho)
         self.imagem.fill(VERMELHO)
         self.imagem_retangulo = self.imagem.get_rect()
@@ -84,22 +89,78 @@ class Bola:
         self.imagem_retangulo.x = tela_retangulo.centerx
         self.imagem_retangulo.y = tela_retangulo.centery
         self.velo = [x, y]
+        self.pos = list(tela_retangulo.center)
+
+    def colide_parede(self):
+        score1 = score2 = 0
+        if self.imagem_retangulo.y < 0 or self.imagem_retangulo.y > tela_retangulo.bottom - self.altura:
+            self.velo[1] *= -1
+
+        if self.imagem_retangulo.x < 0 or self.imagem_retangulo.x > tela_retangulo.right - self.largura:
+            self.velo[0] *= -1
+            if self.imagem_retangulo.x == 0:
+                score2 += 1
+            if self.imagem_retangulo.x == 800:
+                score1 += 1
+
+    def colide_raquetea(self, raquete_rect):
+        if self.imagem_retangulo.colliderect(raquete_rect):
+            # self.velo *= -1
+            print('Boa')
+
+    def colide_raqueteb(self, raquete_rect):
+        if self.imagem_retangulo.colliderect(raquete_rect):
+            # self.velo *= -1
+            print('Boa')
 
     def move(self):
-        self.imagem_retangulo[0] += self.velo[0] * self.velocidade
-        self.imagem_retangulo[1] += self.velo[1] * self.velocidade
+        self.pos[0] += self.velo[0] * self.velocidade
+        self.pos[1] += self.velo[1] * self.velocidade
+        self.imagem_retangulo.center = self.pos
 
-    def atualiza(self):
+    def atualiza(self, raquete_rect):
+        self.colide_parede()
+        self.colide_raquetea(raquete_rect)
+        self.colide_raqueteb(raquete_rect)
         self.move()
-        self.imagem_retangulo.clamp_ip(tela_retangulo)
 
     def realiza(self):
         tela.blit(self.imagem, self.imagem_retangulo)
 
-# Tamanho         
+# Placar Jogador 1
+class Placara:
+    def __init__(self):
+        pygame.font.init()
+        self.fonte = pygame.font.Font(None, 36)
+        self.pontos = 10
+
+    def contagem(self):
+        self.text = self.fonte.render('Pontos = ' + str(self.pontos), 1, (255, 255, 255))
+        self.textpos = self.text.get_rect()
+        self.textpos = tela.get_width()
+        tela.blit(self.text, self.textpos)
+        tela.blit(tela, (0, 0))
+
+# Placar Jogador 2
+class Placarb:
+    def __init__(self):
+        pygame.font.init()
+        self.fonte = pygame.font.Font(None, 36)
+        self.pontos = 10
+
+    def contagem(self):
+        self.text=self.fonte.render('Pontos = ' + str(self.pontos), 1, (255, 255, 255))
+        self.textpos = self.text.get_rect()
+        self.textpos = tela.get_width()
+        tela.blit(self.text, self.textpos)
+        tela.blit(tela, (0, 0))
+
+# Tamanho
 raquetea = Raquete1((10, 100))
 raqueteb = Raquete2((10, 100))
 bola = Bola((15, 15))
+placar1 = Placara
+placar2 = Placarb
 
 # Jogo
 while not fim:
@@ -113,5 +174,8 @@ while not fim:
     bola.realiza()
     raquetea.atualiza(tecla)
     raqueteb.atualiza(tecla)
-    bola.atualiza()
+    bola.atualiza(raquetea.imagem_retangulo)
+    bola.atualiza(raqueteb.imagem_retangulo)
+    placar1.contagem()
+    placar2.contagem()
     pygame.display.update()
